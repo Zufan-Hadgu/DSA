13a3380b9f54f0b2dd7096b2b86d710864d325f8
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2026-03-10 21:29:16 +0300

    Time: 4 ms (33.02%) | Memory: 21.1 MB (87.32%) - LeetSync

---
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        count = 0
+        rows = len(board)
+        cols = len(board[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == ".":
+                    continue
+                if i > 0 and board[i-1][j] == "X":
+                    continue
+                if j > 0 and board[i][j-1] == "X":
+                    continue
+                count += 1
+                
+        return count
