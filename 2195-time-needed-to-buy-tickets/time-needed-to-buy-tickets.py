fc0f55457bbbcda7089293cce1dbf47a25698d05
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2026-06-21 00:37:05 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (22.79%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        target = tickets[k]
+        time = 0
+
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], target)
+            else:
+                time += min(tickets[i], target - 1)
+
+        return time
+        
