f44d07be1d9c08ce054ab1cd282b6652ad096c48
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2026-09-14 05:10:26 +0300

    Time: 603 ms (20.44%) | Memory: 19.1 MB (88.92%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,14 +1,14 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         total = n
-        output = 0
+        ans = 0
         for i in range(1,n + 1):
             total = total - i
             if total >= 0:
-                output += 1
+                ans += 1
             else:
                 break
-        return output
+        return ans
     
 
 
